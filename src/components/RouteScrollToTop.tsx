91ce8e5fc2cbb45745247de9a868d00dcf8eba44
kingsleywebtech <kingsleyanaab604@gmail.com>
2026-08-29 00:26:28 +0000

    updated

--- src/components/RouteScrollToTop.tsx
+++ src/components/RouteScrollToTop.tsx
@@ -1,11 +1,38 @@
-import { useEffect } from 'react'
+import { useEffect, useLayoutEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export function RouteScrollToTop() {
   const { pathname, search } = useLocation()
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+    if (!('scrollRestoration' in window.history)) {
+      return
+    }
+
+    const previous = window.history.scrollRestoration
+    window.history.scrollRestoration = 'manual'
+
+    return () => {
+      window.history.scrollRestoration = previous
+    }
+  }, [])
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+
+    const frame = window.requestAnimationFrame(() => {
+      window.scrollTo(0, 0)
+    })
+
+    return () => window.cancelAnimationFrame(frame)
+  }, [pathname, search])
+
+  useEffect(() => {
+    const timer = window.setTimeout(() => {
+      window.scrollTo(0, 0)
+    }, 0)
+
+    return () => window.clearTimeout(timer)
   }, [pathname, search])
 
   return null
